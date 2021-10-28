5b69b538c1f9aa16cce4af21342cd824af7c0676
Carlos Diaz <carlosd2mil@gmail.com>
2021-10-28 18:15:47 -0500

    Documentacion

--- Snake/snake.py
+++ Snake/snake.py
@@ -8,55 +8,57 @@ Exercises
 4. Change the snake to respond to mouse clicks.
 
 """
-
+#Librerias
 from random import randrange
 from turtle import *
-
 from freegames import square, vector
 
+#Vector de velocidad cada objeto
 food = vector(0, 0)
-snake = [vector(10, 0)]
-aim = vector(0, -10)
+snake = [vector(10, 0)] #La serpiente se mueve cada 10 px
+aim = vector(0, 10)    #Empieza con direccion -y
 
-
+#Cambio de direccion
 def change(x, y):
     "Change snake direction."
-    aim.x = x
-    aim.y = y
+    aim.x = x                               #Cambiar direccion horizontal
+    aim.y = y                               #Cambiar direccion vertical
 
-
+#Verificar si la serpiente esta dentro del playground
 def inside(head):
     "Return True if head inside boundaries."
-    return -200 < head.x < 190 and -200 < head.y < 190
+    return -200 < head.x < 190 and -200 < head.y < 190  #Si head esta dentro de los limites -200<x<190, -200<y<190
 
-
+#Mover a la serpiente
 def move():
     "Move snake forward one segment."
-    head = snake[-1].copy()
-    head.move(aim)
+    head = snake[-1].copy()                 #La cabeza esta un paso adelante de donde estaba antes
+    head.move(aim)                          #La cabeza se mueve en la direccion aim
 
-    if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
+    #Si choca
+    if not inside(head) or head in snake:   #Si out of boundaries o choca con si misma
+        square(head.x, head.y, 9, 'red')    #Pintar de rojo el cuadro de la cabeza
         update()
         return
 
     snake.append(head)
 
+    #Si comio
     if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
+        print('Snake:', len(snake))         #Print longitud de la serpiente
+        food.x = randrange(-15, 15) * 10    #Generar comida dentro y en coordenadas aleatorias
         food.y = randrange(-15, 15) * 10
     else:
-        snake.pop(0)
+        snake.pop(0)                        #Elimina la ultima posicion de la serpiente
 
     clear()
 
-    for body in snake:
-        square(body.x, body.y, 9, 'black')
+    for body in snake:                      #Para el tamaño de la serpiente
+        square(body.x, body.y, 9, 'black')  #Cuadro del cuerpo en cords x, y, tamaño, color
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, 'green')      #Cuadro de la comida en cords x, y, tamaño, color
     update()
-    ontimer(move, 100)
+    ontimer(move, 50)                       #Delay/Velocidad del juego - Default:100
 
 
 setup(420, 420, 370, 0)
